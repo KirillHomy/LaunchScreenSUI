f8088eda7d5c6b9b46864a3269400006eaf052a3
Kirill Khomicevich <83289514+KirillHomy@users.noreply.github.com>
2025-01-21 18:25:47 +0200

    Initial Commit

---
+++ LaunchScreenSUI/LaunchScreenSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreenSUIApp.swift
+//  LaunchScreenSUI
+//
+//  Created by Kirill Khomicevich on 21.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct LaunchScreenSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
